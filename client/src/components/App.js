cf6f45fb926b1381075927447d02fcbaa116aaef
Justin Harr <justin@Justins-MBP.lan>
2018-09-21 15:56:48 -0400

    heroku build

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,15 @@ import Landing from './Landing';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const App = () => {
   return (
@@ -24,4 +33,4 @@ const App = () => {
   );
 };
 
-export default Landing;
+export default App;
