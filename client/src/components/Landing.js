e0f6edf19a6dc9e3dae4d1f5556517487e3265cc
Justin Harr <justin@Justins-MBP.lan>
2018-09-25 22:16:03 -0400

    revamp marketing landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,14 +7,15 @@ import product2 from "../assets/prod2.png";
 import star from "../assets/star.png";
 import starempty from "../assets/star-empty.png";
 import affiliateDash from "../assets/affiliatedash.png";
-import heroProdImage1 from "../assets/hero-prod-1.png";
-import heroProdImage2 from "../assets/hero-prod-2.png";
-import heroProdImage3 from "../assets/hero-prod-3.png";
+import heroProdImage1 from "../assets/photo_camera_1.svg";
+import heroProdImage2 from "../assets/watch.svg";
+import heroProdImage3 from "../assets/computer.svg";
 
 const heroImages = [
-  {imageSrc: heroProdImage1, color: '#FF9E9D'},
-  {imageSrc: heroProdImage2, color: '#3FB8AF'},
-  {imageSrc: heroProdImage3, color: '#FFC48C'}];
+  { imageSrc: heroProdImage1, color: "#00B14F" },
+  { imageSrc: heroProdImage2, color: "#FF3D1E" },
+  { imageSrc: heroProdImage3, color: "#781EA7" }
+];
 
 class Landing extends React.Component {
   constructor(props) {
@@ -26,6 +27,26 @@ class Landing extends React.Component {
     };
     this.timer = this.timer.bind(this);
   }
+   shadeColor = (color, percent) => {
+
+    let R = parseInt(color.substring(1,3),16);
+    let G = parseInt(color.substring(3,5),16);
+    let B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    let RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    let GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    let BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+}
   componentDidMount() {
     var intervalId = setInterval(this.timer, 2000);
     // store intervalId in the state so it can be accessed later:
@@ -38,60 +59,76 @@ class Landing extends React.Component {
   }
 
   timer() {
-    let newHeroImage = this.state.heroImageIndex == heroImages.length - 1 ? 0 : this.state.heroImageIndex + 1;
+    let newHeroImage =
+      this.state.heroImageIndex == heroImages.length - 1
+        ? 0
+        : this.state.heroImageIndex + 1;
     // setState method is used to update the state
-    this.setState({ heroImageIndex: newHeroImage, heroImage: heroImages[newHeroImage]  });
+    this.setState({
+      heroImageIndex: newHeroImage,
+      heroImage: heroImages[newHeroImage]
+    });
   }
   render() {
-    let {heroImageIndex, heroImage} = this.state;
-    let {imageSrc, color} = heroImage;
+    let { heroImageIndex, heroImage } = this.state;
+    let { imageSrc, color } = heroImage;
 
     return (
       <div className="landing">
-        <LandingHeader />
-        <div className="hero-wrapper">
-          {false && (
-            <div className="on-board">
-              <input className="email" placeholder="Email" />
-              <button className="subscribe">Subscribe</button>
-            </div>
-          )}
-          <div className="main">
-            <div className="main-left">
-              <div style ={{backgroundColor : color}} className="hero">
-                <img src={imageSrc} />
+        <div style={{ backgroundColor: this.shadeColor(color, 30) }} className="landing-main">
+          <LandingHeader />
+          <div className="hero-wrapper">
+            {false && (
+              <div className="on-board">
+                <input className="email" placeholder="Email" />
+                <button className="subscribe">Subscribe</button>
               </div>
-              <div className="wrapper">
-                <span className="beacon" />
-                <span className="beacon" />
-                <span className="beacon" />
-                <span className="beacon" />
-                <span className="beacon blink">
-                  <img src={icon} />
-                </span>
+            )}
+            <div className="browser">
+              <div className="browser-header">
+                <div className="browser-button red" />
+                <div className="browser-button yellow" />
+                <div className="browser-button green" />
               </div>
-            </div>
-            <div className="main-right">
-              <div className="prod-name" />
-              <div className="sub-title" />
-              <div className="text-line" />
-              <div className="text-line" />
-              <div className="text-line" />
-              <div className="column-wrapper">
-                <div className="left-column">
-                  <div className="text-line" />
-                  <div className="text-line" />
-                  <div className="text-line" />
+              <h4 className="prod-page-header">Easily add to any product page</h4>
+              <div className="main">
+                <div className="main-left">
+                  <div style={{ backgroundColor: color }} className="hero">
+                    <img src={imageSrc} />
+                  </div>
+                  <div className="wrapper">
+                    <span className="beacon" />
+                    <span className="beacon" />
+                    <span className="beacon" />
+                    <span className="beacon" />
+                    <span className="beacon blink">
+                      <img src={icon} />
+                    </span>
+                  </div>
                 </div>
-                <div className="right-column" />
+                <div className="main-right">
+                  <div className="prod-name" />
+                  <div className="sub-title" />
+                  <div className="text-line" />
+                  <div className="text-line" />
+                  <div className="text-line" />
+                  <div className="column-wrapper">
+                    <div className="left-column">
+                      <div className="text-line" />
+                      <div className="text-line" />
+                      <div className="text-line" />
+                    </div>
+                    <div className="right-column" />
+                  </div>
+                  <div className="buyButton" />
+                </div>
               </div>
-              <div className="buyButton" />
             </div>
           </div>
         </div>
         <div class="feature">
           <h2 className="feature-header">Customer Dashboard</h2>
-          <h4 class="feature-subheader">
+          <h4 className="feature-subheader">
             The customer dashboard will provide direct access to their favorite
             products linking directly back to your site, reducing churn an
             increasing conversion.
