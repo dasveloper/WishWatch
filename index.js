4427a76adaf171f202ccecde3b510472d95cac31
Justin Harr <justin@Justins-MacBook-Pro.local>
2018-10-24 22:36:56 -0400

    broken commit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const authRoutes = require('./routes/authRoutes');
-const subscribeRoutes = require('./routes/subscribeRoutes');
-const affiliateRoutes = require('./routes/affiliateRoutes');
-const productRoutes = require('./routes/productRoutes');
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -37,10 +34,11 @@ if (process.env.NODE_ENV === 'production'){
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
+
+
+
 authRoutes(app);
-subscribeRoutes(app);
-affiliateRoutes(app);
-productRoutes(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
